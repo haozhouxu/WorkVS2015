03f775b39628f82bec46db67a71d0dac74c215b7
haozhouxu <470001421@qq.com>
2015-10-14 11:46:34 +0800

    新建

---
+++ ConsoleApplication_FileStream/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication_FileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream F = new FileStream("test.dat",FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            for (int i = 1; i <= 20; i++)
+            {
+                F.WriteByte((byte)i);
+            }
+
+            F.Position = 0;
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write(F.Position + "---");
+                Console.WriteLine(F.ReadByte()+" ");
+            }
+            F.Close();
+            Console.ReadKey();
+        }
+    }
+}
