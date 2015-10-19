134005bbecb0b486f33eaf56391cabfaf7a14991
haozhouxu <470001421@qq.com>
2015-10-19 15:53:32 +0800

    新增MVVM

---
+++ WpfApplication_MVVM/Models/StudentModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication_MVVM.Models
+{
+    public class StudentModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 学号
+        /// </summary>
+        private int studentId;
+        public int StudentId
+        {
+            get
+            {
+                return studentId;
+            }
+            set
+            {
+                studentId = value;
+                NotifyPropertyChanged("StudentId");
+            }
+        }
+
+        /// <summary>  
+        /// 姓名  
+        /// </summary>  
+        private string studentName;
+        public string StudentName
+        {
+            get
+            {
+                return studentName;
+            }
+            set
+            {
+                studentName = value;
+                NotifyPropertyChanged("StudentName");
+            }
+        }
+
+        /// <summary>  
+        /// 年龄  
+        /// </summary>  
+        private int studentAge;
+        public int StudentAge
+        {
+            get
+            {
+                return studentAge;
+            }
+            set
+            {
+                studentAge = value;
+                NotifyPropertyChanged("StudentAge");
+            }
+        }
+
+        /// <summary>  
+        /// Email  
+        /// </summary>  
+        private string studentEmail;
+        public string StudentEmail
+        {
+            get
+            {
+                return studentEmail;
+            }
+            set
+            {
+                studentEmail = value;
+                NotifyPropertyChanged("StudentEmail");
+            }
+        }
+
+        /// <summary>  
+        /// 性别  
+        /// </summary>  
+        private string studentSex;
+        public string StudentSex
+        {
+            get
+            {
+                return studentSex;
+            }
+            set
+            {
+                studentSex = value;
+                NotifyPropertyChanged("StudentSex");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ WpfApplication_MVVM/ViewModels/StudentViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Input;
+using WpfApplication_MVVM.Models;
+
+namespace WpfApplication_MVVM.ViewModels
+{
+    public class StudentViewModel
+    {
+        public DelegateCommand ShowCommand { get; set; }
+        public StudentModel Student { get; set; }
+        public StudentViewModel()
+        {
+            Student = new StudentModel();
+            ShowCommand = new DelegateCommand();
+            ShowCommand.ExecuteCommand = new Action<object>(ShowStudentData);
+        }
+        private void ShowStudentData(object obj)
+        {
+            Student.StudentId = 1;
+            Student.StudentName = "tiana";
+            Student.StudentAge = 20;
+            Student.StudentEmail = "8644003248@qq.com";
+            Student.StudentSex = "大帅哥";
+        }
+
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        public Action<object> ExecuteCommand = null;
+        public Func<object, bool> CanExecuteCommand = null;
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteCommand != null)
+            {
+                return this.CanExecuteCommand(parameter);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this.ExecuteCommand != null)
+            {
+                this.ExecuteCommand(parameter);
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
