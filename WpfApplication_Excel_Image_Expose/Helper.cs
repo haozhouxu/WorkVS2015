b87d36410c33da925134c0a9047887fcb5e331ff
haozhouxu <470001421@qq.com>
2016-01-26 18:14:58 +0800

    增加数据导入

--- WpfApplication_Excel_Image_Expose/Helper.cs
+++ WpfApplication_Excel_Image_Expose/Helper.cs
@@ -11,9 +11,40 @@ using System.Windows.Media.Imaging;
 
 namespace WpfApplication_Excel_Image_Expose
 {
-    public class Helper
+    public class ExcelHelper
     {
+        public static string GetCellValue(WorkbookPart wbPart, Cell theCell)
+        {
+            string value = theCell.InnerText;
+            if (theCell.DataType != null)
+            {
+                switch (theCell.DataType.Value)
+                {
+                    case CellValues.SharedString:
+                        var stringTable = wbPart.
+                          GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+                        if (stringTable != null)
+                        {
+                            value = stringTable.SharedStringTable.
+                              ElementAt(int.Parse(value)).InnerText;
+                        }
+                        break;
 
+                    case CellValues.Boolean:
+                        switch (value)
+                        {
+                            case "0":
+                                value = "FALSE";
+                                break;
+                            default:
+                                value = "TRUE";
+                                break;
+                        }
+                        break;
+                }
+            }
+            return value;
+        }
     }
 
     public class PictureInfo
@@ -22,5 +53,292 @@ namespace WpfApplication_Excel_Image_Expose
         public int FromCol { get; set; }
         public Image Image { get; set; }
         public string RefId { get; set; }
+    }
+
+    public class Jewelry
+    {
+        #region 字段
+        private string _guid;
+        private Image _image;
+        private string _buyTime;
+        private Double _buyPrice;
+        private Double _goldPrice;
+        private string _type;
+        private string _color;
+        private string _mark;
+        private string _buySource;
+
+        private string _state;
+
+        private string _saleTime;
+        private string _saleWho;
+        private Double _salePirce;
+
+        private string _row;
+        private string _col;
+
+        #endregion
+
+        #region 属性
+
+        public string Guid
+        {
+            get
+            {
+                return _guid;
+            }
+
+            set
+            {
+                if (_guid != value)
+                {
+                    _guid = value;
+                }
+            }
+        }
+
+        public Image Image
+        {
+            get
+            {
+                return _image;
+            }
+
+            set
+            {
+                if (_image != value)
+                {
+                    _image = value;
+                }
+            }
+        }
+
+        public string BuyTime
+        {
+            get
+            {
+                return _buyTime;
+            }
+
+            set
+            {
+                if (_buyTime != value)
+                {
+                    _buyTime = value;
+                }
+            }
+        }
+
+        public Double BuyPrice
+        {
+            get
+            {
+                return _buyPrice;
+            }
+
+            set
+            {
+                if (_buyPrice != value)
+                {
+                    _buyPrice = value;
+                }
+            }
+        }
+
+        public double GoldPrice
+        {
+            get
+            {
+                return _goldPrice;
+            }
+
+            set
+            {
+                if (_goldPrice != value)
+                {
+                    _goldPrice = value;
+                }
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+
+            set
+            {
+                if (_type != value)
+                {
+                    _type = value;
+                }
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+
+            set
+            {
+                if (_color != value)
+                {
+                    _color = value;
+                }
+            }
+        }
+
+        public string Mark
+        {
+            get
+            {
+                return _mark;
+            }
+
+            set
+            {
+                if (_mark != value)
+                {
+                    _mark = value;
+                }
+            }
+        }
+
+        public string BuySource
+        {
+            get
+            {
+                return _buySource;
+            }
+
+            set
+            {
+                if (_buySource != value)
+                {
+                    _buySource = value;
+                }
+            }
+        }
+        
+        public string SaleTime
+        {
+            get
+            {
+                return _saleTime;
+            }
+
+            set
+            {
+                if (_saleTime != value)
+                {
+                    _saleTime = value;
+                }
+            }
+        }
+
+        public string SaleWho
+        {
+            get
+            {
+                return _saleWho;
+            }
+
+            set
+            {
+                if (_saleWho != value)
+                {
+                    _saleWho = value;
+                }
+            }
+        }
+
+        public Double SalePirce
+        {
+            get
+            {
+                return _salePirce;
+            }
+
+            set
+            {
+                if (_salePirce != value)
+                {
+                    _salePirce = value;
+                }
+            }
+        }
+        
+
+        public string State
+        {
+            get
+            {
+                return _state;
+            }
+
+            set
+            {
+                if (_state != value)
+                {
+                    _state = value;
+                }
+            }
+        }
+
+        public string Row
+        {
+            get
+            {
+                return _row;
+            }
+
+            set
+            {
+                if (_row != value)
+                {
+                    _row = value;
+                }
+            }
+        }
+
+        public string Col
+        {
+            get
+            {
+                return _col;
+            }
+
+            set
+            {
+                if (_col != value)
+                {
+                    _col = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region 方法
+        public Jewelry()
+        {
+
+        }
+
+        public Jewelry(bool isNew)
+        {
+            if (isNew)
+            {
+                _guid = System.Guid.NewGuid().ToString();
+                Image = new Image();
+            }
+        }
+        #endregion
+
     }
 }
--- WpfApplication_Excel_Image_Expose/MainWindow.xaml.cs
+++ WpfApplication_Excel_Image_Expose/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using DocumentFormat.OpenXml.Drawing.Spreadsheet;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Drawing.Spreadsheet;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace WpfApplication_Excel_Image_Expose
 {
@@ -26,7 +28,9 @@ namespace WpfApplication_Excel_Image_Expose
         {
             InitializeComponent();
 
-            Expose();
+            //Expose();
+
+            table();
         }
 
         public void Expose()
@@ -101,6 +105,305 @@ namespace WpfApplication_Excel_Image_Expose
             //dataGridView1.AutoGenerateColumns = true;
             it1.ItemsSource = pic1;
             //dataGridView1.ItemsSource = pic1;
+        }
+
+        public void ex()
+        {
+            //string path = @"C:\xhz\综合.xlsx";
+            //using (SpreadsheetDocument document = SpreadsheetDocument.Open(path, false))
+            //{
+            //    string version = string.Empty;
+            //    WorkbookPart wbPart = document.WorkbookPart;
+            //    List<Sheet> sheets = wbPart.Workbook.Descendants<Sheet>().ToList();
+            //    var versionSheet = wbPart.Workbook.Descendants<Sheet>().FirstOrDefault(c => c.Name == "Version");
+            //    WorksheetPart worksheetPart = (WorksheetPart)wbPart.GetPartById(versionSheet.Id);
+
+            //    if (versionSheet == null)
+            //        //throw new Exceptions.ValidationException("There must be Version sheet !");
+
+            //    Cell theCell = worksheetPart.Worksheet.Descendants<Cell>().Where(c => c.CellReference.Value == "A2").FirstOrDefault();
+            //    string type = string.Empty;
+            //    if (theCell != null)
+            //    {
+            //        version = ExcelHelper.GetCellValue(wbPart, theCell);
+            //    }
+            //    else
+            //    {
+            //        //throw new Exceptions.ValidationException("Uploading file does not have version number!");
+            //    }
+            //}
+        }
+
+        public void table()
+        {
+            try
+            {
+                string filePath = @"C:\xhz\综合.xlsx";
+                //根据Excel流转换为spreadDocument对象
+                using (SpreadsheetDocument spreadDocument = SpreadsheetDocument.Open(filePath, false))//Excel文档包
+                {
+                    //Workbook workBook = spreadDocument.WorkbookPart.Workbook;//主文档部件的根元素
+                    //Sheets sheeets = workBook.Sheets;//块级结构（如工作表、文件版本等）的容器
+                    WorkbookPart workBookPart = spreadDocument.WorkbookPart;
+                    //获取Excel中SheetName集合
+                    List<string> sheetNames = GetSheetNames(workBookPart);
+
+                    //遍历每个sheet页
+                    foreach (string sheetName in sheetNames)
+                    {
+                        IEnumerable<Row> sheetRows = GetWorkBookPartRows(workBookPart, sheetName);
+                        if (sheetRows == null || sheetRows.Count() <= 0)
+                        {
+                            return;
+                        }
+
+                        //将数据导入DataTable,假定第一行为列名,第二行以后为数据
+                        ObservableCollection<Jewelry> oje = new ObservableCollection<Jewelry>();
+                        foreach (Row row in sheetRows)
+                        {
+                            //获取Excel中的列头
+                            if (row.RowIndex == 1)
+                            {
+
+                            }
+                            else
+                            {
+                                //Excel第二行同时为DataTable的第一行数据
+                                //DataRow dataRow = GetDataRow(row, dataTable, workBookPart);
+                                //if (dataRow != null)
+                                //{
+                                //    dataTable.Rows.Add(dataRow);
+                                //}
+
+                                IEnumerable<Cell> cells = row.Elements<Cell>();
+                                Jewelry je = new Jewelry(true);
+                                string year = string.Empty;
+                                string month = string.Empty;
+                                foreach (Cell cell in cells)
+                                {
+                                    je.Row = cell.CellReference.Value.Substring(1);
+                                    je.Col = cell.CellReference.Value.Substring(0, 1);
+                                    string cellVlue = GetCellValue(cell, workBookPart);
+                                    switch (je.Col)
+                                    {
+                                        case "A":
+                                            year = cellVlue;
+                                            break;
+                                        case "B":
+                                            month = cellVlue;
+                                            DateTime dt = new DateTime(int.Parse(year), int.Parse(month), 1);
+                                            je.BuyTime = dt.ToString("s");
+                                            break;
+                                        case "C":
+                                            break;
+                                        case "D":
+                                            break;
+                                        case "E":
+                                            je.GoldPrice = Double.Parse(cellVlue);
+                                            break;
+                                        case "F":
+                                            je.Type = cellVlue;
+                                            break;
+                                        case "G":
+                                            je.Color = cellVlue;
+                                            break;
+                                        case "H":
+                                            if (cellVlue.Equals("是"))
+                                            {
+                                                je.State = "卖出";
+                                            }
+                                            else
+                                            {
+                                                je.State = "未卖";
+                                            }
+                                            break;
+                                        case "I":
+                                            je.SaleWho = cellVlue;
+                                            break;
+                                        case "J":
+                                            if (cellVlue.Length == 8)
+                                            {
+                                                DateTime dt1 = new DateTime(int.Parse(cellVlue.Substring(0, 4)), int.Parse(cellVlue.Substring(4, 2)), int.Parse(cellVlue.Substring(6, 2)));
+                                                je.SaleTime = dt1.ToString("s");
+                                            }
+                                            else if (cellVlue.Length == 4)
+                                            {
+                                                DateTime dt2 = new DateTime(int.Parse(cellVlue.Substring(0, 4)), 1, 1);
+                                                je.SaleTime = dt2.ToString("s");
+                                            }
+                                            break;
+                                        case "K":
+                                            je.Mark = cellVlue;
+                                            break;
+                                        case "L":
+                                            //je.BuySource = cellVlue;
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                }
+                                oje.Add(je);
+                            }
+                        }
+                        string s = "";
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                //throw new Exception("可能Excel正在打开中,请关闭重新操作！");
+            }
+        }
+
+        private List<string> GetSheetNames(WorkbookPart workBookPart)
+        {
+            List<string> sheetNames = new List<string>();
+            Sheets sheets = workBookPart.Workbook.Sheets;
+            foreach (Sheet sheet in sheets)
+            {
+                string sheetName = sheet.Name;
+                if (!string.IsNullOrEmpty(sheetName))
+                {
+                    sheetNames.Add(sheetName);
+                }
+            }
+            return sheetNames;
+        }
+
+        /// <summary>
+        /// 根据WorkbookPart和sheetName获取该Sheet下所有Row数据
+        /// </summary>
+        /// <param name="workBookPart">WorkbookPart对象</param>
+        /// <param name="sheetName">SheetName</param>
+        /// <returns>该SheetName下的所有Row数据</returns>
+        public IEnumerable<Row> GetWorkBookPartRows(WorkbookPart workBookPart, string sheetName)
+        {
+            IEnumerable<Row> sheetRows = null;
+            //根据表名在WorkbookPart中获取Sheet集合
+            IEnumerable<Sheet> sheets = workBookPart.Workbook.Descendants<Sheet>().Where(s => s.Name == sheetName);
+            if (sheets.Count() == 0)
+            {
+                return null;//没有数据
+            }
+
+            WorksheetPart workSheetPart = workBookPart.GetPartById(sheets.First().Id) as WorksheetPart;
+            //获取Excel中得到的行
+            sheetRows = workSheetPart.Worksheet.Descendants<Row>();
+
+            return sheetRows;
+        }
+
+        public string GetCellValue(Cell cell, WorkbookPart workBookPart)
+        {
+            string cellValue = string.Empty;
+            if (cell.ChildElements.Count == 0)//Cell节点下没有子节点
+            {
+                return cellValue;
+            }
+            string cellRefId = cell.CellReference.InnerText;//获取引用相对位置
+            string cellInnerText = cell.CellValue.InnerText;//获取Cell的InnerText
+            cellValue = cellInnerText;//指定默认值(其实用来处理Excel中的数字)
+
+            //获取WorkbookPart中NumberingFormats样式集合
+            List<string> dicStyles = GetNumberFormatsStyle(workBookPart);
+            //获取WorkbookPart中共享String数据
+            SharedStringTable sharedTable = workBookPart.SharedStringTablePart.SharedStringTable;
+
+            try
+            {
+                EnumValue<CellValues> cellType = cell.DataType;//获取Cell数据类型
+                if (cellType != null)//Excel对象数据
+                {
+                    switch (cellType.Value)
+                    {
+                        case CellValues.SharedString://字符串
+                                                     //获取该Cell的所在的索引
+                            int cellIndex = int.Parse(cellInnerText);
+                            cellValue = sharedTable.ChildElements[cellIndex].InnerText;
+                            break;
+                        case CellValues.Boolean://布尔
+                            cellValue = (cellInnerText == "1") ? "TRUE" : "FALSE";
+                            break;
+                        case CellValues.Date://日期
+                            cellValue = Convert.ToDateTime(cellInnerText).ToString();
+                            break;
+                        case CellValues.Number://数字
+                            cellValue = Convert.ToDecimal(cellInnerText).ToString();
+                            break;
+                        default: cellValue = cellInnerText; break;
+                    }
+                }
+                else//格式化数据
+                {
+                    if (dicStyles.Count > 0 && cell.StyleIndex != null)//对于数字,cell.StyleIndex==null
+                    {
+                        int styleIndex = Convert.ToInt32(cell.StyleIndex.Value);
+                        string cellStyle = string.Empty;
+                        if (dicStyles.Count >= styleIndex)
+                        {
+                            cellStyle = dicStyles[styleIndex - 1];//获取该索引的样式
+                        }
+                        if (cellStyle.Contains("yyyy") || cellStyle.Contains("h")
+                            || cellStyle.Contains("dd") || cellStyle.Contains("ss"))
+                        {
+                            //如果为日期或时间进行格式处理,去掉“;@”
+                            cellStyle = cellStyle.Replace(";@", "");
+                            while (cellStyle.Contains("[") && cellStyle.Contains("]"))
+                            {
+                                int otherStart = cellStyle.IndexOf('[');
+                                int otherEnd = cellStyle.IndexOf("]");
+
+                                cellStyle = cellStyle.Remove(otherStart, otherEnd - otherStart + 1);
+                            }
+                            double doubleDateTime = double.Parse(cellInnerText);
+                            DateTime dateTime = DateTime.FromOADate(doubleDateTime);//将Double日期数字转为日期格式
+                            if (cellStyle.Contains("m")) { cellStyle = cellStyle.Replace("m", "M"); }
+                            if (cellStyle.Contains("AM/PM")) { cellStyle = cellStyle.Replace("AM/PM", ""); }
+                            cellValue = dateTime.ToString(cellStyle);//不知道为什么Excel 2007中格式日期为yyyy/m/d
+                        }
+                        else//其他的货币、数值
+                        {
+                            //cellStyle = cellStyle.Substring(cellStyle.LastIndexOf('.')== -1?0: cellStyle.LastIndexOf('.') - 1).Replace("\\", "");
+                            //decimal decimalNum = decimal.Parse(cellInnerText);
+                            //cellValue = decimal.Parse(decimalNum.ToString(cellStyle)).ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                //string expMessage = string.Format("Excel中{0}位置数据有误,请确认填写正确！", cellRefId);
+                //throw new Exception(expMessage);
+                cellValue = "N/A";
+            }
+            return cellValue;
+        }
+
+        private List<string> GetNumberFormatsStyle(WorkbookPart workBookPart)
+        {
+            List<string> dicStyle = new List<string>();
+            Stylesheet styleSheet = workBookPart.WorkbookStylesPart.Stylesheet;
+            OpenXmlElementList list = styleSheet.NumberingFormats.ChildElements;//获取NumberingFormats样式集合
+
+            foreach (var element in list)//格式化节点
+            {
+                if (element.HasAttributes)
+                {
+                    using (OpenXmlReader reader = OpenXmlReader.Create(element))
+                    {
+                        if (reader.Read())
+                        {
+                            if (reader.Attributes.Count > 0)
+                            {
+                                string numFmtId = reader.Attributes[0].Value;//格式化ID
+                                string formatCode = reader.Attributes[1].Value;//格式化Code
+                                dicStyle.Add(formatCode);//将格式化Code写入List集合
+                            }
+                        }
+                    }
+                }
+            }
+            return dicStyle;
         }
 
     }
