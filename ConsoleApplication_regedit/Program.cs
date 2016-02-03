9f91f13db72ac6979429db35604cdd2caa646499
haozhouxu <470001421@qq.com>
2016-02-03 17:43:49 +0800

    增加测试注册表项目

---
+++ ConsoleApplication_regedit/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_regedit
+{
+    class Program
+    {
+        public const string AppPaths = "Software\\深圳市致远速联信息技术有限公司";
+
+        static void Main(string[] args)
+        {
+            RegistryKey currentKey = null;
+            RegistryKey keys = Registry.LocalMachine.OpenSubKey(AppPaths, false);
+            foreach (string item in keys.GetSubKeyNames())
+            {
+                currentKey = keys.OpenSubKey(item);
+                var path = currentKey.GetValue("Path");
+                var version = currentKey.GetValue("Version");
+            }
+        }
+    }
+}
